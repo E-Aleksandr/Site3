b806ac807c63b52f17f412d1be99ef8cf07ab09f
E-Aleksandr <git.AleksandraE@gmail.com>
2026-04-26 16:50:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const { createClient } = require('@libsql/client');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
